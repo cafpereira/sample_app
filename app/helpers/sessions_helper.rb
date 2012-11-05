e84ec62a00de769dd7b7ed24929a199e75024330
Carlos Pereira <cpereira@groupon.com>
2012-11-05 12:12:33 -0800

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,4 +34,11 @@ module SessionsHelper
 		redirect_to(session[:return_to] || default)
 		session.delete(:return_to)
 	end
+
+	def signed_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in." 
+		end
+	end
 end
